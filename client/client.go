31e3d15f699effad9df2271ea1e8d7640bc2b850
shubhamyadav-trademo <subhamyadav580@gmail.com>
2024-10-03 00:34:05 +0530

    adding LICENSE file and new logger

--- client/client.go
+++ client/client.go
@@ -1,21 +1,31 @@
 package client
 
 import (
-	"log"
 	"net/http"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
+
+var log = logrus.New()
 
 // TrackRequestTime is middleware to track the time taken for each HTTP request
 func TrackRequestTime(next http.Handler) http.Handler {
+	log.Formatter = &logrus.JSONFormatter{}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		startTime := time.Now()
-
 		// Call the next handler
 		next.ServeHTTP(w, r)
-
 		// Log the time taken
 		duration := time.Since(startTime)
-		log.Printf("Request to %s took %v", r.URL.Path, duration)
+		log.WithFields(logrus.Fields{
+			"authKey":   "",
+			"RPM":       100,
+			"CPU Usage": "50%",
+			"url":       r.URL.Path,
+			"duration":  duration,
+		}).Info("Request tracked")
+		// log.Printf("Request to %s took %v", r.URL.Path, duration)
 	})
 }
